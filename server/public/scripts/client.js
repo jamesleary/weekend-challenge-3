7a34dfa76746014bbf127d0a2d7be80d8d30cd3d
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-07-08 18:18:45 -0500

    added styling to buttons and competled button functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,24 +6,27 @@ $(document).ready(function(){
 });
 function deleteTasks(taskId){
   $.ajax({
-        url: '/tasks/' + taskId,
-        type: 'DELETE',
-        success: function(response){
-          console.log(response);
-          getTasks();
-        } // end success
-      }); //end ajax
+    url: '/tasks/' + taskId,
+    type: 'DELETE',
+    success: function(response){
+      console.log(response);
+      getTasks();
+    } // end success
+  }); //end ajax
 
 
 
 }
 function clickHandlers(){
   $('#addButton').on('click', function(){
+
     console.log('addButton click');
     var objectToSend = {
       task: $('#taskIn').val(),
     };
+
     addTasks(objectToSend);
+    $('#taskIn').val('') ;
   });
   $('#viewTasks').on('click', '.deleteBtn', function(){
     console.log('Delete Task');
@@ -33,6 +36,7 @@ function clickHandlers(){
   });
   $('#viewTasks').on('click', '.completeBtn', function(){
     console.log('Complete Task');
+
     var compid = $(this).data('compid');
     console.log($(this));
     completeTasks(compid);
@@ -42,30 +46,31 @@ function addTasks(newTask){
   console.log( 'in addTask', newTask );
   //ajax request to send new task to the database
   $.ajax({
-  url: '/tasks',
-  type: 'POST',
-  data: newTask,
-  success: function( response ){
-    console.log('got some tasks: ', response );
-    getTasks();
-  } // end success
-}); //end ajax
+    url: '/tasks',
+    type: 'POST',
+    data: newTask,
+    success: function( response ){
+      console.log('got some tasks: ', response );
+      getTasks();
+    } // end success
+  }); //end ajax
 }
 
 function completeTasks(compid){
   $.ajax({
-        url: '/tasks/' + compid,
-        type: 'PUT',
-        success: function(response){
-          console.log(response);
-          getTasks();
-        } // end success
-      }); //end ajax
+    url: '/tasks/' + compid,
+    type: 'PUT',
+    success: function(response){
+      console.log(response);
+      getTasks();
+    } // end success
+  }); //end ajax
 }
 
 // get task list from server from database
 function getTasks(){
   $('#viewTasks').empty();
+
   console.log('in getTasks');
   $.ajax({
     url: '/tasks',
@@ -78,14 +83,18 @@ function getTasks(){
 }
 //Append to viewTask container, items come from database and put on HTML
 function appendToDom (tasks){
-  console.log('Appending to DOM');
+  console.log('Appending to DOM' ,tasks);
   for (var i = 0; i < tasks.length; i++) {
     var task = tasks[i];
     var $tr = $('<tr></tr>');
-    $tr.append('<td class="edittask">'+ task.task +'</td>');
+if (task.complete == "false") {
+  $tr.append('<td class="edittask">'+ task.task +'</td>');
+}else {
+  $tr.append('<td class="edittask" id ="completedTask">'+ task.task +'</td>');
+}
 
-    $tr.append('<td>'+ '<button class ="completeBtn"data-compid="' + task.id+ '">Complete</button"</td>');
-    $tr.append('<td>'+ '<button class ="deleteBtn" data-taskid="' + task.id + '">Delete</button"</td>');
+    $tr.append('<td>'+ '<button class ="completeBtn"data-compid="' + task.id+ '">✔</button"</td>');
+    $tr.append('<td>'+ '<button class ="deleteBtn" data-taskid="' + task.id + '">X</button"</td>');
     $('#viewTasks').append($tr);
   }
 }
